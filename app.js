17c2dca5747fb2b31ac374d628fdc03bca10a680
Mike <m_kanyar@encs.concordia.ca>
2020-05-18 21:27:47 -0400

    got the date from the computer

--- app.js
+++ app.js
@@ -8,34 +8,14 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   var today = new Date();
   currentDay = today.getDay();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+  var day = today.toLocaleDateString("en-US", options);
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-
-    default:
-      console.log("error the current day value is " + currentDay);
-      break;
-  }
   //kindOfDay is the exact same attribute in list.ejs
   res.render("list", { kindOfDay: day });
 });
