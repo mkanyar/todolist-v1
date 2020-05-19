fa41a6da61eee2cd587150d6cb463b9bfbe8d263
Mike <m_kanyar@encs.concordia.ca>
2020-05-19 03:20:06 -0400

    adding the about page

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.post("/", function (req, res) {
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItem: workItems });
 });
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 // app.post("/work", function (req, res) {
 //   let item = req.body.newListItem;
 //   workItems.push(item);
